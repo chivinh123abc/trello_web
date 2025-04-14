248e9a7b63127b4628ff9fd03cf38e5abb48812b
chivinh123abc <mail.lcvinh@gmail.com>
2025-04-14 21:39:00 +0700

    React Router Dom Hook Auth 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+
 import Board from '~/pages/Boards/_id'
+import NotFound from '~/pages/404/NotFound'
+import Auth from '~/pages/Auth/Auth'
+
 function App() {
   return (
-    //React Router Dum /boards /boards/(board_id)
-    <>
+    //React Router Dom /boards /boards/(board_id)
+    <Routes>
+      {/* Redirect Route */}
+      {/* Ở đây cần replace giá trị true để nó thay thế route /, có thể hiểu là route/ sẽ không còn nằm trong history của Browser
+      // Thực hành dễ hiểu hơn bằng cách nhấn Go Home từ trang 404 xong thử quay lại bằng nút back của trình duyệt giữa 2 trường hợp có replace hoặc không có
+       */}
+      <Route path='/' element={
+        <Navigate to='/boards/67f255a06c5aa0fcdb33f6c6' replace={true} />
+      } />
       {/* Board Detail */}
-      <Board />
-    </>
+      <Route path='/boards/:boardId' element={<Board />} />
+
+      {/* Authetication */}
+      <Route path='/login' element={<Auth />} />
+      <Route path='/register' element={<Auth />} />
+
+      {/* 404 not found page */}
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
