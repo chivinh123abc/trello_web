e4b67f6a0250995559f3d18e8fa3b02ec2403953
chivinh123abc <mail.lcvinh@gmail.com>
2025-05-11 18:21:54 +0700

    Update Board Invitation, Real Time Socket.io

--- src/main.jsx
+++ src/main.jsx
@@ -29,10 +29,11 @@ const persistor = persistStore(store)
 //Ky thuat InjectStore
 injectStore(store)
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/'>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter basename='/'>
         <CssVarsProvider theme={theme}>
           <ConfirmProvider defaultOptions={{
             allowClose: false,
@@ -52,7 +53,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <ToastContainer theme='colored' position='bottom-left' />
           </ConfirmProvider>
         </CssVarsProvider>
-      </PersistGate>
-    </Provider>
-  </BrowserRouter >
+      </BrowserRouter >
+    </PersistGate>
+  </Provider>
 )
